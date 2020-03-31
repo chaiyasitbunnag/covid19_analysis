3fcd1d4fff28918b7abff7c6061a6466c1324c75
inkchaiyasit <tenability1990@gmail.com>
2020-04-01 03:46:07 +0700

    log lag5 plot

--- analysis_script.R
+++ analysis_script.R
@@ -1,9 +1,13 @@
 library(ggplot2)
+library(jsonlite)
+library(plotly)
 library(dplyr)
 library(tidyr)
 library(bigrquery)
 library(httr)
 library(ggmap)
+library(lubridate)
+library(RColorBrewer)
 
 ##### importing data from bigquery #####
 ## 1. connect to public dataset: https://console.cloud.google.com/marketplace/details/bigquery-public-datasets/covid19-dataset-list
@@ -82,9 +86,9 @@ distinct(summary_data,date) %>% arrange(date)
 url <- paste0("https://maps.googleapis.com/maps/api/geocode/json?address=Japan&key=",api_key)
 res <- GET(url,encode = "json")
 geo_info <- rjson::fromJSON(file = url)
+geo_info$results[[1]]$geometry$location$lat
 geo_info$results[[1]]$geometry$location$lng
-
-## loop countries to get coordinates
+## loop if too many countries' lat lon missing
 
 summary2 <- 
 summary_data %>%
@@ -99,4 +103,78 @@ summary_data %>%
 summary2 %>% group_by(country_region,date) %>% tally() %>% arrange(-n)
 summary2 %>% filter(country_region=="Thailand"&date==as.Date("2020-03-29"))
 summary2 %>% filter(country_region=="US"&date==as.Date("2020-03-29"))
+us <- summary2 %>% filter(country_region=="US")
+us_china <- summary2 %>% filter(country_region=="US"|country_region=="China"|country_region=="Mainland China")
+us_china$country_region[us_china$country_region=="Mainland China"] <- "China"
+## test us
+format(Sys.Date(), format = "%Y-%b-%d")
+Sys.setlocale("LC_TIME", "English")
+
+g <- 
+   us_china %>% 
+   group_by(country_region,date) %>% 
+   summarise(total_confirmed=sum(confirmed)) %>%
+   mutate(new = total_confirmed - lag(total_confirmed,n=1,default = first(total_confirmed))) %>% 
+   arrange(country_region,date) %>% 
+   mutate(new_lag5 = new - lag(new,n=5,default = first(total_confirmed))) %>% 
+   ungroup()
+
+   ggplot(g,aes(x=date,y=log2(new_lag5)))+
+   geom_point(aes(col = country_region))+
+   geom_smooth(aes(group = country_region,col=country_region))+
+   theme(legend.position = "top")
+   
+   ggplot(g,aes(x=date,y=new_lag5))+
+   geom_point(aes(col = country_region))+
+   geom_smooth(aes(group = country_region,col=country_region,se=F))+
+   theme(legend.position = "top")
+   
+lag5_nonlog<- 
+   ggplot(g,aes(x=date,y=new_lag5))+
+   geom_point(aes(col = country_region))+
+   geom_smooth(aes(group = country_region,col=country_region),se=F)+
+   theme_minimal()+
+   theme(legend.position = "top",
+         panel.grid.minor.x = element_blank(),
+         panel.grid.major.y = element_blank(),
+         panel.grid.minor.y = element_blank())
+set.seed(999)
+lag5_log<- 
+   ggplot(g,aes(x=date,y=log10(new_lag5)))+
+   geom_point(aes(col = country_region),alpha=0.3,size=2)+
+   geom_smooth(aes(group = country_region,col=country_region),se=F)+
+   scale_color_manual(values = sample(colors(distinct = T),2))+
+   theme_minimal()+
+   theme(legend.position = "top",
+         panel.grid.minor.x = element_blank(),
+         panel.grid.major.y = element_blank(),
+         panel.grid.minor.y = element_blank())
+   
+lag5_nonlog
+lag5_log
+
+
+ggplotly(g2) %>% hide_legend()
+
+us_china_lag3 <- 
+   us_china %>% 
+   group_by(country_region,date) %>% 
+   summarise(total_confirmed=sum(confirmed)) %>%
+   mutate(new = total_confirmed - lag(total_confirmed,n=1,default = first(total_confirmed))) %>%
+   group_by(country_region,date) %>% 
+   mutate(new_lag3 = new - lag(new,n=3,default = first(new))) %>% 
+   arrange(country_region,date)
+   
+ 
+   ggplot(us_china_lag3,aes(x=date,y=new))+
+   geom_point(aes(col = country_region))+
+   geom_smooth(aes(group = country_region,col=country_region))+
+   theme(legend.position = "top")
+
+g <-
+   us %>%
+   group_by(country_region,date) %>% summarise(total_confirmed=sum(confirmed)) %>% arrange(date) %>% 
+   ggplot(aes(x = date, y = total_confirmed))+geom_line()
+
+ggplotly(g)
 
